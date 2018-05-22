96a4069b8e25e061254a8082a30ba4974a907351
abbet <abbet.christian@gmail.com>
2018-05-22 20:48:35 +0200

    overlay button

--- assets/js/glass.js
+++ assets/js/glass.js
@@ -4,10 +4,15 @@ var data_lens;
 var data_outer;
 
 
-var raw_html =  '<div class="4u{2}"><span class="image fit"><div>' +
-                '<img src="{0}" style="width:100%;">' +
-                '<div class="text-block">{1}</div>' +
-                '</div></span></div>';
+var raw_html =  '<div class="4u{2}">' +
+                    '<span class="image fit"><div>' +
+                    '<img style="position:absolute; visibility: hidden" src="data/test.png">' +
+                    '<img src="{0}">' +
+                    '<div class="text-block">{1}</div>' +
+                    '<input onclick="toggle_overlay(this)" type="checkbox" id="demo-copy-{3}">' +
+                    '<label for="demo-copy-{4}">Overlay</label>' +
+                    '</div></span>' +
+                '</div>';
 
 $.getJSON("data/json/outer.json", function(json) {
     data_outer = json;
@@ -22,6 +27,13 @@ $.getJSON("data/json/connector.json", function(json) {
     data_connector = json;
 });
 
+function toggle_overlay(param) {
+    if(param.checked) {
+        param.parentElement.getElementsByTagName('img')[0].style.visibility = "visible"
+    } else {
+        param.parentElement.getElementsByTagName('img')[0].style.visibility = "hidden"
+    }
+}
 
 function display_preview() {
     var e = document.getElementById("demo-glass-names");
@@ -71,7 +83,9 @@ function display_sub_ratio(data, id_name, id_select) {
             .replace('{2}', n)
             .replace('{0}', 'data/' + id_name + '/' + data['names'][data['args'][id_select][i]] + '.jpg')
             .replace('{1}', data['names'][data['args'][id_select][i]]
-                + '<br>' + 'score: ' + 100*data['values'][id_select][i].toFixed(3)+'%')
+                + '<br>' + 'score: ' + (100*data['values'][id_select][i]).toFixed(1)+'%')
+            .replace('{3}', id_name + i.toString())
+            .replace('{4}', id_name + i.toString())
     }
     document.getElementById("display-result-" + id_name).innerHTML = text;
 
